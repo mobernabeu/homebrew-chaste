8a21cc4684ee5c663a64148e2255587c74ca34cb
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2014-12-08 20:25:18 +0000

    Try splitting into two formulas

--- chaste.rb
+++ chaste.rb
@@ -4,16 +4,7 @@ class Chaste < Formula
   homepage "http://www.cs.ox.ac.uk/chaste"
   head 'https://github.com/chaste/chaste.git'
   
-  depends_on "xsd"
-  depends_on "hdf5189" => "enable-parallel"
-  depends_on "boost149"
-  depends_on "scons"
-  depends_on "xerces-c"
-  depends_on "petsc"
-  depends_on "parmetis" 
-  depends_on "metis"
-  depends_on "vtk5"
-  depends_on "hypre" => :optional
+  depends_on "chaste-dependencies"
 
   def install
     system "scons"
