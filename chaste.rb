f66686e9e9dd6207989ab47b9444c0c7f9d4c447
Miguel O. Bernabeu <miguel.bernabeu@ucl.ac.uk>
2014-12-08 23:09:12 +0000

    Reverting previous two commits. Wrong approach

--- chaste.rb
+++ chaste.rb
@@ -4,7 +4,16 @@ class Chaste < Formula
   homepage "http://www.cs.ox.ac.uk/chaste"
   head 'https://github.com/chaste/chaste.git'
   
-  depends_on "chaste-dependencies"
+  depends_on "xsd"
+  depends_on "hdf5" => "enable-parallel"
+  depends_on "boost149"
+  depends_on "scons"
+  depends_on "xerces-c"
+  depends_on "petsc"
+  depends_on "parmetis" 
+  depends_on "metis"
+  depends_on "vtk5"
+  depends_on "hypre" => :optional
 
   def install
     system "scons"
